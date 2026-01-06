96060225ab0321667ea37db1e4708d46b8c65bb9
KatP <Hailey.Pascarella@vtsd.com>
2026-01-06 11:46:06 -0500

    rt

--- 1.2.3/main.py
+++ 1.2.3/main.py
@@ -1,5 +1,6 @@
 import turtle as trtl
 
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 apple_image = "apple.gif" # Store the file name of your shape
 ground_height = -200
 apple_letter_x_offset = -25
@@ -12,15 +13,21 @@ wn.addshape(apple_image) # Make the screen aware of the new file
 
 wn.bgpic("background.gif")
 apple = trtl.Turtle()
+wn.tracer(False)
 apple.penup()
 
 # given a turtle, set that turtle to be shaped by the image file
 def draw_apple(active_apple):
   active_apple.shape(apple_image)
-  #draw_letter("A", active_apple)
+  draw_letter("A", active_apple)
+  wn.update()
 
 def drop_apple():
+  wn.tracer(True)
   apple.goto(apple.xcor(), ground_height)
+  apple.clear()
+  apple.hideturtle()
+  wn.tracer(False)
 
 def draw_letter(letter, active_apple):
   active_apple.color("white")
@@ -31,6 +38,5 @@ def draw_letter(letter, active_apple):
 
 draw_apple(apple)
 wn.onkeypress(drop_apple, "a")
-
 wn.listen()
 trtl.mainloop()
